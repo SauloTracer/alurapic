90aa151c487041b8d461fca87a35ccf55a760e09
Saulo Machado <saulo.tracer@gmail.com>
2020-10-25 00:30:13 -0300

    Nomeando rotas; definindo rota defalut; acessando rotas por nome no router-link da página de cadastro de forma que mesmo que endereço da rota mude, se o nome continuar o mesmo não quebra o link.

--- alurapic/src/routes.js
+++ alurapic/src/routes.js
@@ -2,6 +2,7 @@ import Cadastro from './components/cadastro/Cadastro.vue'
 import Home from './components/home/Home.vue'
 
 export const routes = [
-	{path: '', component: Home, title: 'Home'},
-	{path: '/cadastro', component: Cadastro, title: 'Cadastro'}
+	{path: '', name: 'home', component: Home, title: 'Home'},
+	{path: '/cadastro',  name: 'cadastro', component: Cadastro, title: 'Cadastro'},
+	{path: '*', component: Home, hidden: true}
 ];
